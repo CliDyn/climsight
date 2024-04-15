af063e4bb3dc484e7a5a86267c19d9ff177cdb84
Ivan Kuznetsov <ivan.kuznetsov@gmail.com>
2024-04-15 16:56:01 +0200

    modified Dockerfile to fit to a new structure; add option skipLLMCall to docker

--- src/climsight/launch.py
+++ src/climsight/launch.py
@@ -1,11 +1,24 @@
 import subprocess
 import os
+import sys
 
 def launch_streamlit():
     # Get the directory where launch.py is located
     launch_dir = os.path.dirname(os.path.realpath(__file__))
+   
     # Construct the absolute path to climsight.py
     climsight_path = os.path.join(launch_dir, "climsight.py")
+    
+    # Check if 'skipLLMCall' argument is provided
+    skip_llm_call = 'skipLLMCall' in sys.argv
 
+    # Prepare the command to run
+    command = ["streamlit", "run", climsight_path]    
+    
+    if skip_llm_call:
+        climsight_path = climsight_path + " skipLLMCall"
+    if skip_llm_call:
+        command.append("skipLLMCall") 
+        
     # Run the command with the absolute path
-    subprocess.run(["streamlit", "run", climsight_path])
+    subprocess.run(command)
