b14e36c06c048535e62c0c9461a8c10ac2cce825
AntoniaJost <antonia.jost@web.de>
2024-08-02 15:39:57 +0200

    refactored code to match coding standards

--- src/climsight/rag.py
+++ src/climsight/rag.py
@@ -7,15 +7,23 @@ from langchain_openai.embeddings import OpenAIEmbeddings
 from langchain_chroma import Chroma
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(
+   filename='climsight.log',
+   level=logging.INFO,
+   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+   datefmt='%Y-%m-%d %H:%M:%S'
+)
 
 # global variables
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 EMBEDDING_MODEL = "text-embedding-3-large"
 CHROMA_PATH = "rag_db"
 
+
 # local variables
 document_path = './data/ipcc_text_reports/'
 timestamp_file = './data/last_update.txt'
+
 
 def get_file_mod_times(folder_path):
     """
@@ -35,41 +43,77 @@ def get_file_mod_times(folder_path):
     return mod_times
 
 
+def get_file_names(folder_path):
+    """
+    Gets the names of all text files in a folder. Throws an error if the
+    folder contains other files beyond .txt.
+
+    Params:
+    folder_path (str): name of the folder where all files are stored.
+
+    Returns:
+    file_names (list): list of all file names.
+    """
+    try:
+        # Check if all files are text files
+        for file in os.listdir(folder_path):
+            if not file.endswith('.txt'):
+                raise ValueError(f"Non-text file found: {file}")
+
+        # get names of all files
+        files = os.listdir(folder_path)
+        file_names = [file for file in files if os.path.isfile(os.path.join(folder_path, file))]
+        return file_names
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return []
+    
+
+def load_docs(file, encoding='utf-8'):
+    """
+    Loads (and decodes) a text file with langchain textLoader.
+
+    Params:
+    file (str): name of the file that is being loaded.
+    encoding (str): type of encoding of the text file. Default: utf-8
+
+    Returns:
+    documents (list): list of documents loaded
+    """
+    try: 
+        loader = TextLoader(file, encoding=encoding, autodetect_encoding=True)  # autodetect encoding is essential!
+        documents = loader.load()
+        return documents
+    except Exception as e:
+        logger.error(f"Failed to load {file}: {e}")
+        return []
+
+
+def split_docs(documents, chunk_size=2000, chunk_overlap=200, separators=[" ", ",", "\n"]):
+    """
+    Splits the passed documents into chunks.
+
+    Params:
+    documents (list): list of document objects to be split.
+    chunk_size (int): maximum number of characters per chunk. Default: 2000.
+    chunk_overlap (int): number of characters to overlap per chunk. Default: 200.
+    separators (list): list of characters where text can be split. Default: [" ", ",", "\n"]
+
+    Returns:
+    docs (list): list of chunks created from input documents.
+    """
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=separators)
+    docs = text_splitter.split_documents(documents)
+    return docs
+
+
 def initialize_rag_database(force_update=False):
-    # check if api key is available in environment; 
+    # check if api key is available in environment;
     # otherwise no rag initialization (might still use rag if already exists)
     if not OPENAI_API_KEY:
         print("No OpenAI API Key found. Skipping RAG database initialization.")
         return
-    
-    all_documents = []
-
-
-    def get_file_names(folder_path):
-        """
-        Gets the names of all text files in a folder. Throws an error if the
-        folder contains other files beyond .txt.
-
-        Params:
-        folder_path (str): name of the folder where all files are stored.
-
-        Returns:
-        file_names (list): list of all file names.
-        """
-        try:
-            # Check if all files are text files
-            for file in os.listdir(folder_path):
-                if not file.endswith('.txt'):
-                    raise ValueError(f"Non-text file found: {file}")
-
-            # get names of all files
-            files = os.listdir(folder_path)
-            file_names = [file for file in files if os.path.isfile(os.path.join(folder_path, file))]
-            return file_names
-
-        except Exception as e:
-            print(f"Error: {e}")
-            return []
 
     # check if the database needs to be updated
     current_mod_times = get_file_mod_times(document_path)
@@ -86,49 +130,17 @@ def initialize_rag_database(force_update=False):
     file_names = get_file_names(document_path)
     #print(file_names)
 
-
-    def load_docs(file, encoding='utf-8'):
-        """
-        Loads (and decodes) a text file with langchain textLoader.
-
-        Params:
-        file (str): name of the file that is being loaded.
-        encoding (str): type of encoding of the text file. Default: utf-8
-
-        Returns:
-        documents (list): list of documents loaded
-        """
-        loader = TextLoader(file, encoding=encoding, autodetect_encoding=True)  # autodetect encoding is essential!
-        documents = loader.load()
-        return documents
-
-
+    all_documents = []
     for file in file_names:
-        print(file)
-        try:
-            documents = load_docs(os.path.join(document_path, file))
-            all_documents.extend(documents)  # save all of them into one
-        except RuntimeError as e:
-            print(f"Failed to load {file}: {e}")
-
-
-    def split_docs(documents, chunk_size=2000, chunk_overlap=200, separators=[" ", ",", "\n"]):
-        """
-        Splits the passed documents into chunks.
-
-        Params:
-        documents (list): list of document objects to be split.
-        chunk_size (int): maximum number of characters per chunk. Default: 2000.
-        chunk_overlap (int): number of characters to overlap per chunk. Default: 200.
-        separators (list): list of characters where text can be split. Default: [" ", ",", "\n"]
-
-        Returns:
-        docs (list): list of chunks created from input documents.
-        """
-        text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap, separators=separators)
-        docs = text_splitter.split_documents(documents)
-        return docs
-
+        logger.info(f"Processing file: {file}")
+        documents = load_docs(os.path.join(document_path, file))
+        all_documents.extend(documents) # save all of them into one
+        # print(file)
+        # try:
+        #     documents = load_docs(os.path.join(document_path, file))
+        #     all_documents.extend(documents)  # save all of them into one
+        # except RuntimeError as e:
+        #     print(f"Failed to load {file}: {e}")
 
     docs = split_docs(documents=all_documents)
 
@@ -145,7 +157,6 @@ def initialize_rag_database(force_update=False):
             f.write(str(current_mod_times))
     except Exception as e:
         logger.error(f"Failed to initialize the RAG database: {e}")
-
 
 
 # call function to initialize database
